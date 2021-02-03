6ef571db6fd37f7e9484e05060cbe9b328ef1b33
ilanazbel <ilan@autochartist.com>
2021-02-03 06:13:35 -0600

    removed percentaffected and returned 4 different possible outcomes

--- calculatevalue.php
+++ calculatevalue.php
@@ -5,26 +5,24 @@ header('Content-Type: application/json');
 $currentRedeposit = array_key_exists('currentRedeposit', $_GET)?$_GET['currentRedeposit']:null;
 $currentQtyRedeposits = array_key_exists('currentQtyRedeposits', $_GET)?$_GET['currentQtyRedeposits']:null;
 $totalCustomers = array_key_exists('totalCustomers', $_GET)?$_GET['totalCustomers']:null;
-$percentAffected = array_key_exists('percentAffected', $_GET)?$_GET['percentAffected']:null;
 
 if(empty($currentRedeposit) || !is_numeric($currentRedeposit) || $currentRedeposit <= 0)
 {
+    http_response_code(404);
     $response = ["status"=>"failure", "error"=>"currentRedeposit field must be a number > 0"];
 } 
 elseif(empty($currentQtyRedeposits) || !is_numeric($currentQtyRedeposits) || $currentQtyRedeposits <= 0)
 {
+    http_response_code(404);
     $response = ["status"=>"failure", "error"=>"currentQtyRedeposits field must be a number > 0"];
 }
 elseif(empty($totalCustomers) || !is_numeric($totalCustomers) || $totalCustomers <= 0)
 {
+    http_response_code(404);
     $response = ["status"=>"failure", "error"=>"totalCustomers field must be a number > 0"];
 }
-elseif(empty($percentAffected) || !is_numeric($percentAffected) || $percentAffected < 0 || $percentAffected > 1.0)
-{
-    $response = ["status"=>"failure", "error"=>"percentAffected field must be number between 0 and 1"];
-} 
 else {
-
+    $percentAffected;
     $avgRedepositQtyIncrease = 0.25;
     $avgRedepositAmountIncrease = 1.28;
 
@@ -33,10 +31,19 @@ else {
 
     $totalCurrentRedeposit = $currentRedeposit * $currentQtyRedeposits * $totalCustomers;
     $totalNewRedeposit = $newRedeposit * $newQtyRedeposit * $totalCustomers;
-    $diff = round($totalNewRedeposit - $totalCurrentRedeposit);
-    $value = round($diff * $percentAffected);
+    $totalvalue = round($totalNewRedeposit - $totalCurrentRedeposit);
+    $conservative = round($totalvalue * 0.1);
+    $realistic = round($totalvalue * 0.25);
+    $optimistic = round($totalvalue * 0.5);
+    $ludicrous = round($totalvalue * 0.75);
 
-    $response = ["status"=>"success", "data"=>["totalvalue"=>$diff, "affectedvalue"=>$value]];
+    $response = ["status"=>"success", "data"=>[
+        "totalvalue"=>$totalvalue, 
+        "conservative"=>$conservative, 
+        "realistic"=>$realistic, 
+        "optimistic"=>$optimistic, 
+        "ludicrous"=>$ludicrous]
+    ];
 }
 
 echo json_encode($response);
